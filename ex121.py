7bf94bd5f13314f924fd03ab384e3896f57b0644
Clement5789 <90597740+Clement5789@users.noreply.github.com>
2022-01-31 09:46:15 +0100

    Add files via upload

---
+++ ex121.py
@@ -0,0 +1,2 @@
+t=[i % 3 for i in range(100)]
+print(t)
---
+++ ex122.py
@@ -0,0 +1,6 @@
+grille = []
+for i in range (11):
+    for j in range (11):
+        ligne = i*j
+        grille.append ( ligne )
+print (grille)
